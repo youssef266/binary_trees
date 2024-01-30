738ddbcedef60c16be9d0e9491d520b9bf67af2b
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-01-30 19:09:24 +0200

    alx

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@
  *      other wise - a pointer to the new_node
 */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) 
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
