f8c1cfdbbade4f985c6436d5377975d6254de9c0
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-01-30 19:07:26 +0200

    alx

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,22 +1,31 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_left - a function to insert a node as a lift 
+ *
+ * @parent: a pointer to the node to insert the lift child
+ * @value: the value to store in the next new_node
+ * Return: null if the parent is null, null if the new_node is null
+ *      other wise - a pointer to the new_node
+*/
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) 
 {
-	binary_tree_t *new;
+	binary_tree_t *new_node;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
 		return (NULL);
 
 	if (parent->left != NULL)
 	{
-		new->left = parent->left;
-		parent->left->parent = new;
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 	}
-	parent->left = new;
+	parent->left = new_node;
 
-	return (new);
+	return (new_node);
 }
