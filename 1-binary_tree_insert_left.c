1ba8ecd9d0d8f75b64efa97523bb4d2647836bc0
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-01-30 19:00:54 +0200

    alx

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,26 +2,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) 
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new;
 
-    if (parent == NULL) {
-        return (NULL);
-    }
+	if (parent == NULL)
+		return (NULL);
 
-    new_node = binary_tree_node(parent, value);
-    
-    if (new_node == NULL)
-    {
-        return NULL;
-    }
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
 
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        parent->left->parent = new_node;
-    } else {
-        parent->left = new_node;
-    }
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
 
-    return (new_node);
+	return (new);
 }
