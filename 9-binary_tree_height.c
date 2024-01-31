91ac1db5a6325fafb5b8ffe7975dc3153fcb7bad
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-01-31 23:41:52 +0200

    Alx

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - a function that 
+ * binary_tree_height - a function that
  *  Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
  *
